30080d3cc7601ccf2b334c2d85318248b04267d5
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-16 22:08:58 +0530

    Added control structure to the file

---
+++ ControlFlow.py
@@ -0,0 +1,66 @@
+#1. Conditional Statements
+#Control the flow based on conditions.
+#Syntax:
+"""
+if condition:
+    # Code block for true condition
+elif another_condition:
+    # Code block for elif
+else:
+    # Code block for false condition
+"""
+
+#2. Loops
+
+#a. for Loop
+#Used for iterating over a sequence.
+#Syntax:
+"""for item in sequence:
+    # Code block for each iteration"""
+
+#b. while Loop
+#Repeats as long as the condition is true.
+#Syntax:
+"""while condition:
+    # Code block for the loop
+"""
+
+
+#3. Loop Control Statements
+#Modify the flow within loops.
+
+#a. break
+#Exits the loop immediately.
+"""
+for item in sequence:
+    if condition:
+        break
+
+"""
+#b. continue
+#Skips the current iteration and moves to the next.
+"""
+for item in sequence:
+    if condition:
+        continue
+
+"""
+#c. pass
+#Does nothing; a placeholder.
+#Syntax:
+"""
+for item in sequence:
+    pass
+
+"""
+
+#Questions for Practice
+
+"""
+1. Conditional Statements
+Question: Write a program to check if a given number is:
+
+Positive
+Negative
+Zero
+"""
