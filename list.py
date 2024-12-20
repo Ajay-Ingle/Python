8d6230874b747109744bd68adf54a7b6178789b8
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-20 13:14:54 +0530

    Added list file

---
+++ list.py
@@ -0,0 +1,20 @@
+item = ["Bread", "Milk", "Butter", "Cheese"]
+print(item[2])
+print(item)
+item[0]= "Bananna"
+print(item)
+print(item[0:3])
+print(-1)
+
+item.append("Fruits")
+print(item)
+
+item.insert(2, "flour")
+print(item)
+
+
+print(len(item))
+
+"""
+We cannot add the list with string as the data types are different
+"""
