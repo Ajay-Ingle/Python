d50821d6bb6d0d54ec6748b3d1647ab69443c109
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-20 13:50:00 +0530

    Dictonary and Tuple

---
+++ dictonary.py
@@ -0,0 +1,10 @@
+d = {"Aj": 7264, "Ak": 7066, "Ko": 8830, "Papa": 9923, "Mom": 8767}
+x = d["Aj"]
+print(x)
+
+"""Tuples are nothing but list of values grouped together"""
+
+"""
+List: all the values have same meaning(Homogenous)
+Tuples: all the values have different meaning(Heterogenous)
+"""
