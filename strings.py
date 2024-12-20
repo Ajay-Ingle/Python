9ab2fce29cb1daa18fd2ef5d384503b9f1c1e181
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-20 13:02:50 +0530

    strings file

---
+++ strings.py
@@ -0,0 +1,22 @@
+text = "Ajay-Ingle"
+print(text)
+x= text[3]
+print(x)
+
+"""
+Python doesnt allow to change the string as strings are immutable but java does
+"""
+
+"""Creating substring"""
+print("The substring from 0th index to 3rd index is : ",text[0:4])
+print(text[3:]) 
+print(text[:5])
+
+"""MULTIPLE LINE STRING"""
+"""use TRIPLE QUOTE(''')"""
+
+address = '''At post anegaon
+tq. kaij
+dist. beed
+'''
+print( "The multiline string address: ",address)
