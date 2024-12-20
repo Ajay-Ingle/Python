a38caae60137dd7071239cdc0aede7cf42f41e02
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-20 14:06:44 +0530

    Added math module

---
+++ maths.py
@@ -0,0 +1,13 @@
+import math
+import calendar
+print(dir(math))
+
+
+print(math.log10(23))
+print(math.floor(2.3))
+print(math.ceil(2.3))
+
+
+cal = calendar.month(2024,12)
+print(cal)
+print(dir(calendar))
