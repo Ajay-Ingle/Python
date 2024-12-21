dce24e08215564d9139a475ed79fee94ecfb2297
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-21 14:22:57 +0530

    Added multithreadin second stage

--- multithreading.py
+++ multithreading.py
@@ -3,22 +3,30 @@ for a given list of numbers calculate the square and cube of the numbers
 """
 
 import time
+import threading
 
 def calc_square(numbers):
     for i in numbers:
-        print("Square of the numbers")
         time.sleep(0.2)
         print("Square: ", i*i)
 
 def calc_cube(numbers):
     for i in numbers:
-        print("Cube of the numbers")
         time.sleep(0.2)
-        print("Cube: ", i*i)
+        print("Cube: ", i*i*i)
 
 arr = [2,3,8,9]
-calc_square(arr)
-calc_cube(arr)
+
 t = time.time()
 print("The time in sec since this epoch: ",t)
+
+t1 = threading.Thread(target=calc_square, args=(arr,))
+t2 = threading.Thread(target=calc_cube, args=(arr,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
 print("Done in time: ",time.time()-t)
