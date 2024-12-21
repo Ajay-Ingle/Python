3be4c79c4d97dd69fea863d8794723e35037271c
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-21 15:36:42 +0530

    Added lock mechanism

---
+++ multiprocessingCodebasics/lockMechanism.py
@@ -0,0 +1,7 @@
+"""
+just add the mulitprocessing.Lock()
+
+and two functions:
+    lock.acquire() - for adding lock so no other process can acess the resource
+    lock.release()- for releasing the lock so the resource can be used by other process
+"""
