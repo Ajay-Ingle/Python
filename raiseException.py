110dd2c27eb83ffc5fc04fbef3edb818aa54747b
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-22 11:46:36 +0530

    Added finally statment

--- raiseException.py
+++ raiseException.py
@@ -9,4 +9,11 @@ try:
     raise Accident("Crash between cars. ")
 except Accident as e:
     e.print_Exception()
+
+"""
+finally after the except is necessary to handle the exception and execution necessatiy of the code that is necessary for the program.
+
+Suppose dealing with program of the file were the final statment i.e. f.close()
+is necessary to execute the release operation for resources if not executed becasue of some random errors then it will be unusual so we can use this statment for closing the file resources we can use it in the finally: block which makes us sure to execute at all.
+"""
     
