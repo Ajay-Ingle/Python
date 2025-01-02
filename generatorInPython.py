e6b9936437f60e8b6691d4a8da0949b73722ae04
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2025-01-02 23:07:59 +0530

    Added generators in python

---
+++ generatorInPython.py
@@ -0,0 +1,12 @@
+# Generators A generator is a function that yields values one at a time, rather than returning them all at once. It is defined using the yield keyword.
+
+# A generator to produce the first n Fibonacci numbers.
+
+def fibonacci(n):
+    a,b = 0,1
+    for _ in range(n):
+        yield a
+        a,b = b, a + b
+
+for num in fibonacci(5):
+    print(num)
